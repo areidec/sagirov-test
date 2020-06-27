778d9e6894ffb2753205cfdd9eb46cde16747622
Areidec <tatarchuk@grechka.digital>
2020-06-27 17:20:05 +0300

    Updates

---
+++ precache-manifest.89c6758ee141e623126f000547895da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e649dd053a3e2bd4e0d0bb5c318d7ea",
+    "url": "/sagirov-test/index.html"
+  },
+  {
+    "revision": "0bd1ffc9eb5c239ba5f1",
+    "url": "/sagirov-test/static/css/main.3fbbf2af.chunk.css"
+  },
+  {
+    "revision": "bb277d656b42f270ed44",
+    "url": "/sagirov-test/static/js/2.ee31285c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/sagirov-test/static/js/2.ee31285c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd1ffc9eb5c239ba5f1",
+    "url": "/sagirov-test/static/js/main.e140db44.chunk.js"
+  },
+  {
+    "revision": "3e9ff56ea87de935c9a6",
+    "url": "/sagirov-test/static/js/runtime-main.f2ea09e9.js"
+  }
+]);
