66407b995a28cdf36a05c3fe0c56e7856053e086
Areidec <tatarchuk@grechka.digital>
2020-06-29 11:36:16 +0300

    Updates

--- precache-manifest.099e55866156a553511549a3e4bb42a2.js
+++ precache-manifest.099e55866156a553511549a3e4bb42a2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "559856ce34c0c14a8bb939ad8d7e62b9",
+    "revision": "6dd675d0e1d97a2514b78da272884f31",
     "url": "/sagirov-test/index.html"
   },
   {
-    "revision": "941dab4f4a211ef657c0",
+    "revision": "0ad40c630d2f2a8d182a",
     "url": "/sagirov-test/static/css/main.3fbbf2af.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sagirov-test/static/js/2.ee31285c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "941dab4f4a211ef657c0",
-    "url": "/sagirov-test/static/js/main.9f60f11a.chunk.js"
+    "revision": "0ad40c630d2f2a8d182a",
+    "url": "/sagirov-test/static/js/main.4986c525.chunk.js"
   },
   {
     "revision": "3e9ff56ea87de935c9a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sagirov-test/precache-manifest.0137df25bdd8d682b3fded049e5a4d8d.js"
+  "/sagirov-test/precache-manifest.099e55866156a553511549a3e4bb42a2.js"
 );
 
 self.addEventListener('message', (event) => {
